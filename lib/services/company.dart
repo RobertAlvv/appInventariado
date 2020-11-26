aafeaa9d6e5fe3e549eb6ced6aa62b6a7892689d
RobertAlvv <alvarezrobert150@gmail.com>
2020-11-26 17:20:07 -0400

    the response.go file was created which will abstract the requests to the backend

--- lib/services/company.dart
+++ lib/services/company.dart
@@ -1,34 +1,21 @@
-import 'dart:convert';
-import 'package:dio/dio.dart';
 import 'package:inventario_alv/models/company.dart';
 import 'package:inventario_alv/services/response.dart';
 
 class ServiceCompany {
-  final String url = "http://10.0.0.80:3000/api/v1";
-  Dio dio = Dio();
-
-  Future<List<Company>> getCompanies() async {
-    try {
-      Response resp = await dio.get(
-        "$url/companies",
-        options: Options(headers: {
-          "Authorization":
-              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6dHJ1ZSwiZXhwIjoxNjA2NDIzNzQ2LCJzdWIiOiJcciJ9.dXrRXkCThSCjKfs47GdcEx9BGBfk5FySOkY9tK2WaHw"
-        }),
-      );
-      return [];
-    } catch (e) {
-      ResponseError error = ResponseError();
-      String errorStatus = error.status(e.toString());
-      print(errorStatus);
-      print(e);
-      return <Company>[Company(businessName: errorStatus)];
-    }
+  RequestBase request;
+  ServiceCompany() {
+    request = RequestBase();
   }
 
-  // Future<Company> getCompany(String rnc) async {
-  //   final resp = await http.get("$url/company/$rnc");
-  //   Company company = companyFromJson(resp.body);
-  //   return company;
-  // }
+  Future<List<Company>> getCompanies() async {
+    var resp = await request.requestGet("/companies");
+    print(resp);
+    return <Company>[];
+  }
+
+  Future<Company> getCompany(String rnc) async {
+    final resp = await request.requestGet("/companies/$rnc");
+    Company company = companyFromJson(resp.body);
+    return company;
+  }
 }
--- lib/services/response.dart
+++ lib/services/response.dart
@@ -1,7 +1,85 @@
+import 'package:dio/dio.dart';
+
+class RequestBase {
+  String token =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6dHJ1ZSwiZXhwIjoxNjA2NjgzOTAzLCJzdWIiOiJcdTAwMTcifQ.oHJBN34lO6ix-zmp2gq8rDkzHqA8Qa_0wv70gnQ4vbQ";
+
+  BaseOptions baseOptions;
+
+  Dio dio;
+
+  RequestBase() {
+    baseOptions = BaseOptions(
+      baseUrl: "http://10.0.0.80:3000/api/v1",
+      contentType: "application/json",
+      headers: {"Authorization": "Bearer $token"},
+    );
+
+    dio = Dio(baseOptions);
+  }
+
+  ResponseError error = ResponseError();
+
+  Future<dynamic> requestGet(String route) async {
+    try {
+      Response resp = await dio.get(
+        route,
+        options: Options(headers: {"Authorization": "Bearer $token"}),
+      );
+      return resp;
+    } catch (e) {
+      String errorStatus = error.status(e.toString());
+      return errorStatus;
+    }
+  }
+
+  Future<dynamic> requestPost(String route, Map<String, dynamic> data) async {
+    try {
+      Response resp = await dio.post(
+        route,
+        data: data,
+        options: Options(headers: {"Authorization": "Bearer $token"}),
+      );
+      return resp;
+    } catch (e) {
+      String errorStatus = error.status(e.toString());
+      return errorStatus;
+    }
+  }
+
+  Future<dynamic> requestPut(String route, List<dynamic> data) async {
+    try {
+      Response resp = await dio.put(
+        route,
+        data: data,
+        options: Options(headers: {"Authorization": "Bearer $token"}),
+      );
+      return resp;
+    } catch (e) {
+      String errorStatus = error.status(e.toString());
+      return errorStatus;
+    }
+  }
+
+  Future<dynamic> requestDelete(String route, String id) async {
+    try {
+      Response resp = await dio.delete(
+        route,
+        queryParameters: {"id": id},
+        options: Options(headers: {"Authorization": "Bearer $token"}),
+      );
+      return resp;
+    } catch (e) {
+      String errorStatus = error.status(e.toString());
+      return errorStatus;
+    }
+  }
+}
+
 class ResponseError {
   Map<String, String> statusCode = {
     "400": "Missing or Malformed Token",
-    "401": "Invalid or expired Token",
+    "401": "Invalid or Expired Token",
     "404": "Resource Not Found",
     "409": "This Resourse Already Exists",
     "422": "Unprocessable Entity",
